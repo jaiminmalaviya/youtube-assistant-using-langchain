0b6d98be41e4a5a34027a5d61eb61a30dee3e4ab
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2024-01-19 20:30:07 +0530

    first commit

---
+++ langchain_helper.py
@@ -0,0 +1,56 @@
+from langchain_community.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+import os
+
+load_dotenv()
+embeddings = OpenAIEmbeddings()
+
+key = os.getenv("OPENAI_API_KEY")
+
+
+def create_db_from_youtube_video_url(video_url: str) -> FAISS:
+    loader = YoutubeLoader.from_youtube_url(video_url)
+    transcript = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+    docs = text_splitter.split_documents(transcript)
+
+    db = FAISS.from_documents(docs, embeddings)
+    return db
+
+
+def get_response_from_query(db, query, k=5):
+    docs = db.similarity_search(query, k=k)
+    docs_page_content = " ".join([d.page_content for d in docs])
+
+    llm = ChatOpenAI(openai_api_key=key)
+
+    prompt = PromptTemplate(
+        input_variables=["question", "docs"],
+        template="""
+        You are a helpful assistant that that can answer questions about youtube videos 
+        based on the video's transcript.
+        
+        Answer the following question: {question}
+        By searching the following video transcript: {docs}
+        
+        Only use the factual information from the transcript to answer the question.
+        
+        If you feel like you don't have enough information to answer the question, say "I don't know".
+
+        give the answer in correct format, if it required bullet points then use the bullet points, and also add "\n" where break line is needed.
+
+        give the all answer within 100 words and must use hierarchical bullet points. also must add "\n" after every bullet point.
+        """,
+    )
+
+    chain = prompt | llm
+
+    response = chain.invoke({"question": query, "docs": docs_page_content})
+    response = response.content
+    return response, docs
